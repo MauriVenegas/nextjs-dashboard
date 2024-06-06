daf9433dbba0139538e2f7f7cfa4c9570db49984
Mauricio Venegas <mvenegas.developer@gmail.com>
2024-06-05 20:22:00 -0400

    Editar y eliminar invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,23 +1,28 @@
 'use server';
 //⤴ Todo lo que se ejecute aca sere en el servidor
 
-import { z } from 'zod'
+import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-const createInvoiceSchema = z.object({
+// Schema para validar que los datos de invoices cumplan cierta estructura
+const invoiceSchema = z.object({
   id: z.string(),
   customerId: z.string(),
-  amount: z.coerce.number(),
+  amount: z.coerce.number(), //coerce: transforma al amount a number
   date: z.string(),
-  status: z.enum(['pending', 'paid'])
-})
-
-const createInvoiceFromSchema = createInvoiceSchema.omit({
+  status: z.enum(['pending', 'paid']),
+});
+// Elementos a omitir o no considerar
+const createInvoiceFromSchema = invoiceSchema.omit({
   id: true,
-  date: true
-})
+  date: true,
+});
+const updateInvoiceFromSchema = invoiceSchema.omit({
+  id: true,
+  date: true,
+});
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = createInvoiceFromSchema.parse({
@@ -26,16 +31,41 @@ export async function createInvoice(formData: FormData) {
     status: formData.get('status'),
   });
 
-  const amountInCents = amount * 100 // transformamos para evitar errores de redondeo
+  const amountInCents = amount * 100; // transformamos para evitar errores de redondeo
   const date = new Date().toISOString().split('T')[0];
   // const [date] = new Date().toISOString().split('T'); // Otra forma de acceder a la primera posición
 
-  await sql `
+  await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `
+  `;
   // Realiza una recarga de invoices ya que se agrego una nueva
   revalidatePath('/dashboard/invoices');
   // Volvemos a la pagina de invoices
   redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = updateInvoiceFromSchema.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`
+  DELETE FROM invoices WHERE id = ${id}
+  `;
+  revalidatePath('/dashboard/invoices');
 }
